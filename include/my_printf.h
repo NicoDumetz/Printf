6f66deed1fb083de10d268c06d4882785a042632
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 16:53:49 +0100

    [+] adding e specificateur to header

--- include/my_printf.h
+++ include/my_printf.h
@@ -33,6 +33,7 @@ int print_oct(va_list list, int *compt);
 int print_dec(va_list list, int *compt);
 int print_hex(va_list list, int *compt);
 int print_hex_maj(va_list list, int *compt);
+int my_put_sci_nbr(float nb);
 static const disp_t fonc_list[] = {
     {'d', &print_int},
     {'i', &print_int},
@@ -44,6 +45,7 @@ static const disp_t fonc_list[] = {
     {'u', &print_dec},
     {'x', &print_hex},
     {'X', &print_hex_maj},
+    {'e', &print_science},
     {0, NULL}
 };
 
