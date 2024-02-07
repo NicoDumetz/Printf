c0e9c58dbcd30ffa3342d9159da108d47e39d7ed
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 16:54:40 +0100

    [~] fonc ban

--- ressources/point_g.c
+++ ressources/point_g.c
@@ -5,10 +5,11 @@
 ** zgoihzegoih
 */
 #include "my_printf.h"
+#include "../include/my_macro_abs.h"
 
 static void is_negative(double *nb, double *marge)
 {
-    if ( *nb < 0) {
+    if (*nb < 0) {
         *nb *= -1;
         *nb += *marge;
     }
@@ -17,7 +18,7 @@ static void is_negative(double *nb, double *marge)
 static int float_type(double nb, double precision, double *marge)
 {
     char *str;
-    
+
     precision -= my_intlen(nb);
     for (int k = 0; k < precision; k++)
         *marge /= 10;
@@ -27,6 +28,7 @@ static int float_type(double nb, double precision, double *marge)
     str = malloc(precision + 1);
     str[0] = '.';
     for (int k = 0; k < precision; k++) {
+        *marge /= 10;
         nb *= 10;
         str[k + 1] = ((int)nb % 10) + '0';
     }
@@ -37,24 +39,47 @@ static int float_type(double nb, double precision, double *marge)
     return 0;
 }
 
-static int science_type(double nb, double precision, double *marge)
+static int precision_loop(double nb, int precision)
 {
-    for ( int k = 0; my_intlen(nb) != 1; k++) {
-        nb /= 10;
+    char *str;
+
+    str = malloc(precision);
+    for (int k = 0; k < precision - 1; k++) {
+        nb *= 10;
+        str[k] = ((int)nb % 10) + '0';
     }
-    float_type(nb, precision, marge);
-    return 0;
+    for (int k = precision - 2; k >= 0; k--) {
+        if (str[k] > '0' && str[k] <= '9')
+            break;
+        precision--;
+    }
+    return precision - 1;
+}
+
+static int science_precision(double nb, double precision)
+{
+    double mult;
+    double marge = 0.5;
+
+    for (int k = 0; k < precision - 1; k++)
+        marge /= 10;
+    nb = (nb < 0.0) ? -nb : nb;
+    mult = (nb >= 1) ? 0.1 : 10.0;
+    while (my_intlen(nb) != 1)
+        nb *= mult;
+    nb += marge;
+    return precision_loop(nb, precision);
 }
 
 int point_g(double nb)
 {
-    int precision = 6;
+    int precision = 0;
     double marge = 0.5;
 
-    if ( my_intlen(nb) <= precision) {
+    if (my_intlen(ABS(nb)) <= precision && ABS(nb) > 0.0001) {
         float_type(nb, precision, &marge);
     } else {
-        science_type(nb, precision, &marge);
+        my_put_sci_nbr(nb, 0, science_precision(nb, precision));
     }
     return 0;
 }
