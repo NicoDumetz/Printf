f78e58261d2e29615729c7b19ceee5cfd3acb0c5
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 16:54:37 +0100

    [+] point_g.c and my_put_sci_nbr (ajout de précision)

--- ressources/my_put_sci_nbr.c
+++ ressources/my_put_sci_nbr.c
@@ -6,16 +6,23 @@
 */
 #include "my_printf.h"
 
-static int printer(double nb, int nb_mult, int upper)
+static int printer(double nb, int nb_mult, int upper, int precision)
 {
     int counter = 1;
 
-    counter += my_put_float(nb, 6);
+    if (precision) {
+        counter += my_put_float(nb, precision);
+    } else {
+        counter += my_printf("%d", (int)nb);
+    }
     my_putchar((upper) ? 'E' : 'e');
     if (nb_mult >= 0) {
         my_putchar('+');
-        counter++;
+    } else {
+        my_putchar('-');
+        nb_mult = -nb_mult;
     }
+    counter++;
     if (nb_mult <= 9)
         counter += my_put_nbr(0) + 1;
     my_put_nbr(nb_mult);
@@ -23,21 +30,21 @@ static int printer(double nb, int nb_mult, int upper)
     return counter;
 }
 
-int my_put_sci_nbr(double nb, int upper)
+int my_put_sci_nbr(double nb, int upper, int precision)
 {
     int neg;
     double mult;
     int nb_mult = 0;
 
     if (nb == 0.0)
-        return printer(0, 0, upper);
-    neg = (nb < 0) ? 1 : 0;
-    nb = (nb < 0) ? -nb : nb;
+        return printer(0, 0, upper, precision);
+    neg = (nb < 0.0) ? 1 : 0;
+    nb = (nb < 0.0) ? -nb : nb;
     mult = (nb >= 1) ? 0.1 : 10.0;
     while (!(nb >= 1 && nb < 10)) {
         nb = nb * mult;
         nb_mult += (mult > 1) ? -1 : 1;
     }
     nb = (neg) ? -nb : nb;
-    return printer(nb, nb_mult, upper);
+    return printer(nb, nb_mult, upper, precision);
 }
